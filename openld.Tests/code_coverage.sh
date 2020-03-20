571c3c738d38c6ba677538a98ffb3e2896f90263
James Tattersall <10601770+jamerst@users.noreply.github.com>
2020-03-20 16:12:45 +0000

    Update code coverage script

--- openld.Tests/code_coverage.sh
+++ openld.Tests/code_coverage.sh
@@ -1,4 +1,4 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*]openld.Migrations.*%2c[*]openld.Program*%2c[*]openld.Startup*%2c[*]openld.Pages.*%2c[*]openld.Models.*"
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*]openld.Migrations.*%2c[*]openld.Program*%2c[*]openld.Startup*%2c[*]openld.Pages.*%2c[*]openld.Models.*%2c[*]openld.Controllers.*"
 reportgenerator -reports:TestResults/coverage.info -reporttypes:Html -targetdir:TestResults/coverage
 
 xdg-open TestResults/coverage/index.htm
